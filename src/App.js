24f6e7f43d635abc99b272cdacc41ecef43761d6
ahnoldmoon <munnt72@hotmail.com>
2020-01-10 07:07:57 +0900

    add event 1

--- src/App.js
+++ src/App.js
@@ -20,7 +20,9 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state = {
+      mode:'read',
       subject:{title:'WEB', sub:'World Wide Web!'},
+      welcome:{title:'Welcome', desc:'Hello, React!!'},
       contents:[
         {id:1, title:'HTML', desc:'HTML is for information'},
         {id:2, title:'CSS', desc:'CSS is for design'},
@@ -29,6 +31,15 @@ class App extends Component{
     }
   }
   render(){
+    console.log('App render');
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === 'read'){
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return(
       <div>
       <Subject 
@@ -36,7 +47,7 @@ class App extends Component{
         sub={this.state.subject.sub}>
       </Subject>
       <TOC data={this.state.contents}></TOC>
-      <Content title="HTML" desc="HTML is HyperText Markup Language"></Content>
+      <Content title={_title} desc={_desc}></Content>
     </div>
     )
   }
